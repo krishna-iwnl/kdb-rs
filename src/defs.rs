b2200bc35e792c4ba112a537144dbb50c61f697b
Krishna <krshnravipati@gmail.com>
2020-05-19 14:27:06 -0400

    added defs

---
+++ src/defs.rs
@@ -0,0 +1,21 @@
+pub const MAX_ANDS: i8 = 20;
+pub const MAX_ORS: i8 = 20;
+pub const PAGE_SIZE: i32 = 131072;
+
+pub enum Target{
+    Left,
+    Right,
+    Literal,
+}
+
+pub enum CompOperator{
+    LessThan,
+    GreaterThan,
+    Equals,
+}
+
+pub enum Type{
+    Int,
+    Double,
+    String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+pub mod defs;
+pub mod schema;
+
 fn main() {
     println!("Hello, world!");
 }
